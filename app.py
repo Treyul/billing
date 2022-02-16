d6990b69532f73fd061726312eccd9b3f0ecb2e6
Treyul <emmanuelryley55@gmail.com>
2022-02-16 22:45:52 +0300

    adding route configurations and connection to mysql server

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
+from django.shortcuts import redirect
 from flask import Flask,render_template,request
+from mysql.connector import connect,Error
+from getpass import getpass
 
+# set up connection with mysql
+try:
+    with connect(
+        host = "localhost",
+        user = input("Enter username: "),
+        password = getpass("Enter password: "),
+    ) as connection:
+        print("connection")
+except Error as e:
+        print(e)
+
+# set up for routing
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/login",methods=['POST'])
+def login():
+    username = request.form.get("username");
+    password = request.form.get("password")
+    print(username,password)
+    if username == "tre":
+        return render_template("index.html")
 
+@app.route("/")
 def index():
-    render_template("index.html")
+   return render_template("login.html")
