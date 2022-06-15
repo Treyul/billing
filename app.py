570e240a8082d18d6e04ea90b84ee241b3c3153d
Treyul <emmanuelryley55@gmail.com>
2022-06-15 17:59:18 +0300

    minor changes

--- app.py
+++ app.py
@@ -16,11 +16,11 @@ app = Flask(__name__)
 # app.config["MYSQL_USER"] = "root"
 # app.config["MYSQL_PASSWORD"] = "Treyul@18"
 # app.config["MYSQL_DB"] = "water_billing"
+mysql = MySQL()
 app.config["MYSQL_DATABASE_USER"] = "bef134615a5bbe"
 app.config["MYSQL_DATABASE_PASSWORD"] = "70b6c7f2"
 app.config["MYSQL_DATABASE_DB"] = "heroku_ba6afcca4de000d"
 app.config["MYSQL_DATABASE_HOST"] = "us-cdbr-east-05.cleardb.net"
-mysql = MySQL()
 mysql.init_app(app)
 
 # mysql = MySQL(app)
